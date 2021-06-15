41ab35b7041376f81eaff45cf2094bbcc2bcb898
NakUn <chnaan@gmail.com>
2021-06-15 20:56:16 +0900

    mergeTest

--- lib/services/route_names.dart
+++ lib/services/route_names.dart
@@ -8,6 +8,7 @@ class RouteNames {
   static final String memo = 'memo';
   static final String boni = 'boni';
   static final String gyeony = 'gyeony';
+  static final String nakun = 'nakun';
   static final String qrCodeGenerate = 'qrCodeGenerate';
   static final String qrCodeScan = 'qrCodeScan';
   static final String qrCodeResult = 'qrCodeResult';
