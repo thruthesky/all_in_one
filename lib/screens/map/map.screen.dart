18ce10ee6d0d83d15e8973a72b4327ebd3606ea9
JaeHo Song <thruthesky@gmail.com>
2021-06-23 12:20:31 +0900

    문제 해결

--- lib/screens/map/map.screen.dart
+++ lib/screens/map/map.screen.dart
@@ -1,8 +1,6 @@
-import 'package:all_in_one/services/globals.dart';
 import 'package:all_in_one/widgets/layout.dart';
 import 'package:flutter/material.dart';
 import 'map_util/kakaomap_webview.dart';
-import 'package:widgets/widgets.dart';
 
 const String kakaoMapKey = '8def285fc256cfe05281d05f33ded483';
 
@@ -27,7 +25,7 @@ class MapScreen extends StatelessWidget {
               showMapTypeControl: true,
               showZoomControl: true,
               markerImageURL:
-              'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+                  'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
               onTapMarker: (message) async {
                 ScaffoldMessenger.of(context)
                     .showSnackBar(SnackBar(content: Text('Marker is clicked')));
@@ -51,10 +49,8 @@ class MapScreen extends StatelessWidget {
     //     util.getKakaoMapURL(37.402056, 127.108212, name: 'Kakao 본사'));
 
     /// This is short form of the above comment
-    String url =
-    await util.getMapScreenURL(37.402056, 127.108212, name: 'Kakao 본사');
+    String url = await util.getMapScreenURL(37.402056, 127.108212, name: 'Kakao 본사');
 
-    Navigator.push(
-        context, MaterialPageRoute(builder: (_) => KakaoMapScreen(url: url)));
+    Navigator.push(context, MaterialPageRoute(builder: (_) => KakaoMapScreen(url: url)));
   }
 }
