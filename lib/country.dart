fc2e17bf898e92aae58de737215405f5269431a9
JaeHo Song <thruthesky@gmail.com>
2021-06-26 21:28:47 +0900

    Squashed 'packages/subtrees/country_currency_pickers/' content from commit 35ac2f4
    git-subtree-dir: packages/subtrees/country_currency_pickers
    git-subtree-split: 35ac2f4a13e5550c8d8a24e5c9b702a94b3e67e8

---
+++ lib/country.dart
@@ -0,0 +1,26 @@
+class Country {
+  final String? name;
+  final String? isoCode;
+  final String? iso3Code;
+  final String? phoneCode;
+  final String? currencyCode;
+  final String? currencyName;
+
+  Country({
+    this.isoCode,
+    this.iso3Code,
+    this.phoneCode,
+    this.name,
+    this.currencyCode,
+    this.currencyName,
+  });
+
+  factory Country.fromMap(Map<String, String> map) => Country(
+        name: map['name'],
+        isoCode: map['isoCode'],
+        iso3Code: map['iso3Code'],
+        phoneCode: map['phoneCode'],
+        currencyCode: map['currencyCode'],
+        currencyName: map['currencyName'],
+      );
+}
---
+++ lib/utils/utils.dart
@@ -0,0 +1,52 @@
+import 'package:country_currency_pickers/countries.dart';
+import 'package:country_currency_pickers/country.dart';
+import 'package:flutter/widgets.dart';
+
+class CountryPickerUtils {
+  static Country getCountryByIsoCode(String isoCode) {
+    try {
+      return countryList.firstWhere(
+        (country) => country.isoCode!.toLowerCase() == isoCode.toLowerCase(),
+      );
+    } catch (error) {
+      throw Exception("The initialValue provided is not a supported iso code!");
+    }
+  }
+
+  static String getFlagImageAssetPath(String isoCode) {
+    return "assets/${isoCode.toLowerCase()}.png";
+  }
+
+  static Widget getDefaultFlagImage(Country country) {
+    return Image.asset(
+      CountryPickerUtils.getFlagImageAssetPath(country.isoCode!),
+      height: 20.0,
+      width: 30.0,
+      fit: BoxFit.fill,
+      package: "country_currency_pickers",
+    );
+  }
+
+  static Country getCountryByPhoneCode(String phoneCode) {
+    try {
+      return countryList.firstWhere(
+        (country) => country.phoneCode!.toLowerCase() == phoneCode.toLowerCase(),
+      );
+    } catch (error) {
+      throw Exception(
+          "The initialValue provided is not a supported phone code!");
+    }
+  }
+
+  static Country getCountryByCurrencyCode(String currencyCode) {
+    try {
+      return countryList.firstWhere(
+        (country) =>
+            country.currencyCode!.toLowerCase() == currencyCode.toLowerCase(),
+      );
+    } catch (error) {
+      throw Exception(
+          "The initialValue provided is not a supported currency code!");
+    }
+  }
+}
