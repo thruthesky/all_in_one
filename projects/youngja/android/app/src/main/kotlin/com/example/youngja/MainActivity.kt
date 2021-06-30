cc4da4e45d2e29c92588aba6d3e86ecb6552cc0e
JaeHo Song <thruthesky@gmail.com>
2021-06-30 10:41:41 +0900

    remov youngja app

--- projects/youngja/android/app/src/main/kotlin/com/example/youngja/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.youngja
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projects/youngja/lib/services/globals.dart
+++
@@ -1,5 +0,0 @@
-import 'package:youngja/controllers/app.controller.dart';
-import 'package:youngja/services/service.dart';
-
-AppController app = AppController();
-Service service = Service.instance;
