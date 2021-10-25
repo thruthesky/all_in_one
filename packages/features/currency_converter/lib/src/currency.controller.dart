88747b156459590d71ce3fe8cf45bf84e4abb33e
JaeHo Song <thruthesky@gmail.com>
2021-10-25 20:33:27 +0900

    update on ui

--- packages/features/currency_converter/lib/src/currency.controller.dart
+++ packages/features/currency_converter/lib/src/currency.controller.dart
@@ -15,17 +15,31 @@ class CurrencyController extends GetxController {
   void onInit() {
     super.onInit();
 
-    init();
+    loadCurrency();
   }
 
-  init() async {
+  toDouble(dynamic v) {
+    if (v is double)
+      return v;
+    else if (v is int) return v.toDouble();
+    return double.tryParse(v);
+  }
+
+  loadCurrency() async {
     try {
-      final res = await CurrencyApi.instance.get('USD', 'KRW');
+      final res = await CurrencyApi.instance.get(codes[0], codes[1]);
       print('res; $res');
 
-      convert[0] = res['USD_KRW'];
-      convert[1] = res['KRW_USD'];
-      values[1] = convert[0].toStringAsFixed(2);
+      convert[0] = toDouble(res[codes[0] + '_' + codes[1]]);
+      convert[1] = toDouble(res[codes[1] + '_' + codes[0]]);
+
+      if (convert[0] > convert[1]) {
+        values[0] = '1';
+        values[1] = convert[0].toStringAsFixed(2);
+      } else {
+        values[0] = convert[1].toStringAsFixed(2);
+        values[1] = '1';
+      }
       update();
     } catch (e) {
       onError(e);
@@ -39,12 +53,12 @@ class CurrencyController extends GetxController {
     if (i == 0) {
       double v = double.tryParse(values[0]) ?? 0;
       values[1] = (convert[0] * v).toStringAsFixed(2);
+      update(['value1']);
     } else {
       double v = double.tryParse(values[1]) ?? 0;
       values[0] = (convert[1] * v).toStringAsFixed(2);
+      update(['value0']);
     }
-
-    update();
   }
 
   setState(Function ss) {
--- packages/features/currency_converter/lib/src/currency_converter.display.dart
+++ packages/features/currency_converter/lib/src/currency_converter.display.dart
@@ -39,52 +39,65 @@ class CurrencySelect extends StatelessWidget {
   final int i;
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<CurrencyController>(builder: (_) {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          TextButton(
-            onPressed: () {
-              showCurrencyPicker(
-                context: context,
-                showFlag: true,
-                showCurrencyName: true,
-                showCurrencyCode: true,
-                onSelect: (Currency currency) {
-                  print('Select currency: ${currency.name}');
-                  print(currency.code);
-                  _.setState(() {
-                    _.codes[i] = currency.code;
-                    _.symbols[i] = currency.symbol;
-                    _.names[i] = currency.name;
-                  });
-                },
-                favorite: ['USD', 'KRW'],
-              );
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  _.codes[i],
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GetBuilder<CurrencyController>(builder: (_) {
+          return Expanded(
+            flex: 2,
+            child: TextButton(
+              onPressed: () {
+                showCurrencyPicker(
+                  context: context,
+                  showFlag: true,
+                  showCurrencyName: true,
+                  showCurrencyCode: true,
+                  onSelect: (Currency currency) {
+                    print('Select currency: ${currency.name}');
+                    print(currency.code);
+                    _.setState(() {
+                      _.codes[i] = currency.code;
+                      _.symbols[i] = currency.symbol;
+                      _.names[i] = currency.name;
+                    });
+                    _.loadCurrency();
+                  },
+                  favorite: ['USD', 'KRW'],
+                );
+              },
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      _.codes[i],
+                      style: TextStyle(fontSize: 28),
+                    ),
+                    Text('${_.symbols[i]} ${_.names[i]}', style: TextStyle(fontSize: 11)),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
+        SizedBox(width: 16),
+        GetBuilder<CurrencyController>(
+            id: "value$i",
+            builder: (_) {
+              return Expanded(
+                flex: 3,
+                child: TextField(
+                  controller: TextEditingController(text: _.values[i]),
+                  onChanged: (v) {
+                    _.values[i] = v;
+                    _.compute(i);
+                  },
                   style: TextStyle(fontSize: 28),
                 ),
-                Text('${_.symbols[i]} ${_.names[i]}', style: TextStyle(fontSize: 11)),
-              ],
-            ),
-          ),
-          Expanded(
-            child: TextField(
-              controller: TextEditingController(text: _.values[i]),
-              onChanged: (v) {
-                _.values[i] = v;
-                _.compute(i);
-              },
-              style: TextStyle(fontSize: 28),
-            ),
-          ),
-        ],
-      );
-    });
+              );
+            }),
+      ],
+    );
   }
 }
