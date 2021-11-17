53123ac7732bd7fd9bad10e2ae794ab3db0daf24
JaeHo Song <thruthesky@gmail.com>
2021-11-17 11:23:11 +0800

    update download file data from internet

--- packages/services/utils/lib/src/functions.dart
+++ packages/services/utils/lib/src/functions.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
@@ -115,57 +116,73 @@ void printLongString(String text) {
   pattern.allMatches(text).forEach((RegExpMatch match) => print(match.group(0)));
 }
 
-/// download file or data
+/// Download file or data from the Internet and cache it locally.
 ///
 /// [url] is the url to download
 /// [filename] is the file name to save. If it is not set, then it will get the file name from url. The last 64 letters from the end of the url with escape.
 /// [dirPath] is the directory path to save the file. if it is not set, then the file will be saved in temporary folder
-/// [duration] is the time interval to download the file again
-/// [onDownloadBegin], [onDownloadEnd], [onDownload] are the callback on downloda events.
-Future<Uint8List> download(
+/// [expiration] is the time interval to download the file again.
+///   if it is `expiration: Duration(minutes: 500)`, then it will download again after 500 minutes. default is 365 days (a year)
+/// [onDownloadBegin], [onDownloadEnd], [onDownloadProgress] are the callback on downloda events. If the file has already downloaded and not expired, then the callbacks will be not called.
+/// [onDownloadProgress] will be called with -1 if the downloadable size is not known from the backend. onDownloadProgress has three params. first int is for percentage, second int is for how many bytes received, and the third int is for total. If the total is -1, then display the received bytes on screen.
+Future<List<int>> download(
   String url, {
-  Duration? duration,
+  Duration expiration = const Duration(days: 365),
   String? filename,
   String? dirPath,
   Function? onDownloadBegin,
   Function? onDownloadEnd,
-  Function? onDownloadProegress,
+  Function? onDownloadProgress,
 }) async {
-  Directory tempDir = await getTemporaryDirectory();
-  String tempPath = tempDir.path;
-  String pathToSave = tempPath + '/' + 'seoul_art_exhibition7.json';
+  /// Get file path
+  if (dirPath == null) {
+    Directory tempDir = await getTemporaryDirectory();
+    dirPath = tempDir.path;
+  }
+  if (filename == null) {
+    filename = url.length > 64 ? url.substring(url.length - 64) : url;
+    filename = safeFilename(filename);
+  }
+  String pathToSave = dirPath + '/' + filename;
 
+  print('download: $pathToSave');
+
+  /// Check if file exists,
   File file = File(pathToSave);
   bool re = file.existsSync();
 
   if (re) {
-    print('yes, downloaded file exists');
-    // file exists, check if it's older than 5 days,
+    print('---- yes, file exists');
+    // Yes, file exists. Then, check if the file is older than expiration
     DateTime now = DateTime.now();
-    final Duration difference = now.difference(file.lastModifiedSync());
+    DateTime lastModifiedDateTime = file.lastModifiedSync();
+    DateTime expire = lastModifiedDateTime.add(expiration);
 
-    print('Diff in days: ${difference.inDays}');
-    if (difference.inDays > 5) {
-      print('yes, downloaded file is older than 5 days');
+    // The downloaded file has already expired?
+    if (expire.compareTo(now) < 0) {
+      print('---- yes, downloaded file is expired');
       await _downloadUrl(url, pathToSave,
           onDownloadBegin: onDownloadBegin,
           onDownloadEnd: onDownloadEnd,
-          onDownloadProegress: onDownloadProegress);
+          onDownloadProgress: onDownloadProgress);
     }
   } else {
+    // No, file not exists. Download.
+    print('---- No, file not exists. Download.');
     await _downloadUrl(url, pathToSave,
         onDownloadBegin: onDownloadBegin,
         onDownloadEnd: onDownloadEnd,
-        onDownloadProegress: onDownloadProegress);
+        onDownloadProgress: onDownloadProgress);
   }
 
+  /// Read the file
   file = File(pathToSave);
   re = await file.exists();
-
   if (re == false) {
     throw ('ERROR_FAILED_TO_LOAD_TEMPORARY_FILE');
   }
 
+  /// Return the content of the file.
   return file.readAsBytesSync();
 }
 
@@ -174,19 +191,17 @@ _downloadUrl(
   String path, {
   Function? onDownloadBegin,
   Function? onDownloadEnd,
-  Function? onDownloadProegress,
+  Function? onDownloadProgress,
 }) async {
+  print('---- download begins ---- _downloadUrl()');
   if (onDownloadBegin != null) onDownloadBegin();
   Dio dio = Dio();
   final response = await dio.get(
     url,
     onReceiveProgress: (received, total) {
-      if (total != -1) {
-        int p = (received / total * 100).round();
-        print('download progress; $p');
-      } else {
-        print('download progress: total is -1');
-      }
+      if (onDownloadProgress == null) return;
+      int p = (received / total * 100).round();
+      onDownloadProgress(p, received, total);
     },
     // List<int> 로 받기 위해서, 이 옵션 필수.
     // 이렇게 하지 않으면, writeFromSync() 에서 InternalLinkedHashMap<String, dynamic> is not a subtype of type List<int> 에러가 난다.
@@ -198,9 +213,45 @@ _downloadUrl(
 
   File file = File(path);
 
+  /// It saves the data into the file as `List<int>`.
   final raf = file.openSync(mode: FileMode.write);
   raf.writeFromSync(response.data);
   raf.closeSync();
 
   if (onDownloadEnd != null) onDownloadEnd();
+}
+
+/// from https://github.com/2n-1/safe_filename
+String safeFilename(
+  String filename, {
+  String separator = '-',
+  bool withSpaces = false,
+  bool lowercase = false,
+  bool onlyAlphanumeric = false,
+}) {
+  final List<String> reservedCharacters = [
+    '?',
+    ':',
+    '"',
+    '*',
+    '|',
+    '/',
+    '\\',
+    '<',
+    '>',
+    '+',
+    '[',
+    ']',
+  ];
+
+  final RegExp onlyAlphanumericRegex = RegExp(r'''[^a-zA-Z0-9\s.]''');
+
+  String returnString = filename;
+  onlyAlphanumeric
+      ? returnString = returnString.replaceAll(onlyAlphanumericRegex, '')
+      : reservedCharacters.forEach((c) {
+          returnString = returnString.replaceAll(c, separator);
+        });
+  if (!withSpaces) returnString = returnString.replaceAll(' ', separator);
+  return lowercase ? returnString.toLowerCase() : returnString;
 }
